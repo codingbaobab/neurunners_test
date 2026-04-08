ad03e86e5b2d7e525d5ebf272e406f1bc517d229
wonder <wondertanlee@gmail.com>
2026-04-08 14:47:17 +0900

    햄스터 맞나?

---
+++ hamster.js
@@ -0,0 +1,18 @@
+// hamster.js
+
+// 터미널에 출력될 아주 귀여운 햄스터 모양 문자열입니다.
+const hamster = `
+  __      __
+ /  \\____/  \\
+|  __    __  |
+| /  \\  /  \\ |
+| | @|  | @| |   Hello! I'm a tiny hamster!
+| \\__/  \\__/ |   < Feed me some code!
+ \\_ /    \\_ /
+  _|      |_
+ /  \\____/  \\
+|____________|
+`;
+
+// console.log() 함수를 사용해 햄스터를 터미널에 출력합니다.
+console.log(hamster);
